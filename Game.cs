f25e64bbccbba5eae48910c58452caf5e8356457
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-12-13 19:05:44 +0300

    json read from file

--- Game.cs
+++ Game.cs
@@ -7,10 +7,20 @@ namespace tennisSimulation
 {
     class Game
     {
-        public List<Player> players { get; set; }
-       // DataSerializer dataSerializer;
-      
+       
+        // DataSerializer dataSerializer;
+        private readonly string _path = @"C:\Users\Özge Kocaoğlu\source\repos\tennisSimulation\input.json";
 
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader(_path))
+            {
+                string json = r.ReadToEnd();
+                Console.WriteLine(json);
+               Player playerList =JsonConvert.DeserializeObject<Player>(json);
+                Console.WriteLine(playerList);
+            }
+        }
 
         public Game()
         {
@@ -19,14 +29,15 @@ namespace tennisSimulation
 
         public void CreateNewGame()
         {
-            players = new List<Player>();
+            
             CreatePlayers();
+            LoadJson();
         }
 
         //TO DO: JSON Dosyasından çek. 
         public void CreatePlayers()
         {
-            players.Add(new Player(Hand.right.ToString(), 0));
+            /*players.Add(new Player(Hand.right.ToString(), 0));
             players.Add(new Player(Hand.left.ToString(), 5));
             players.Add(new Player(Hand.left.ToString(), 10));
             players.Add(new Player(Hand.right.ToString(), 15));
@@ -36,7 +47,7 @@ namespace tennisSimulation
                 Console.WriteLine("------------------------------------");
                 Console.WriteLine("Player " + i + "\nID: " + players[i].id + "\nHand side: " + players[i].hand + "\nExperience: " + players[i].experience);
             }
-
+            */
         }
 
 
--- Player.cs
+++ Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,7 +10,7 @@ namespace tennisSimulation
         right,
         left
     };
-
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
     class Player
     {
         #region Variables
@@ -20,9 +21,13 @@ namespace tennisSimulation
         #endregion
          
         #region Getter/Setters
+            [JsonProperty(PropertyName ="id")]
             public int id { get; set; }
-            public int experience { get; set; }
+            [JsonProperty(PropertyName = "hand")]
             public string hand { get; set; }
+            [JsonProperty(PropertyName = "experience")]
+            public int experience { get; set; }
+            [JsonProperty(PropertyName = "skills")]
             public PlayerSkills skills { get; set; }
         #endregion
 
